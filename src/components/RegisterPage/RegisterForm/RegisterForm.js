8ab457785139ee9f282b15bb2068b7d69da77de4
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-08-09 17:23:32 +0300

    Adds Register and Login forms

--- src/components/RegisterPage/RegisterForm/RegisterForm.js
+++ src/components/RegisterPage/RegisterForm/RegisterForm.js
@@ -1,8 +1,51 @@
+import { Formik } from "formik";
+import * as yup from "yup";
+import { FormWrapper, FormTitle, Form, FieldWrapper, FormLabel, Field, FormButton } from "./RegisterForm.styled";
+
+const schema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  email: yup.string().email("This is an ERROR email").required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+};
+
 const RegisterForm = () => {
+
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
+  };
+
   return (
-    <>
-      <div></div>
-    </>
+    <FormWrapper>
+      <FormTitle>Sign Up</FormTitle>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        <Form autoComplete="off">
+          <FieldWrapper>
+            <FormLabel htmlFor="name">Name</FormLabel>
+            <Field id="name" name="name" placeholder="Enter your name" />
+          </FieldWrapper>
+          <FieldWrapper>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Field id="email" name="email" type="email" placeholder="Enter email" />
+          </FieldWrapper>
+          <FieldWrapper>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Field id="password" name="password" type="password" placeholder="Enter password" />
+          </FieldWrapper>
+          <FormButton type="submit">Sign Up</FormButton>
+        </Form>
+      </Formik>
+    </FormWrapper>
   );
 };
 export default RegisterForm;
