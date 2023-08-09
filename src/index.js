87f54b0586195b59e224fdcdea8e32c465b99fd6
Oleh Protasevych <protasevicholeg@gmail.com>
2023-08-09 19:08:23 +0300

    fixed router

--- src/index.js
+++ src/index.js
@@ -1,48 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'styles/GlobalStyles';
+import { App } from 'App';
+import theme from 'styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter basename="/teamproject-frontend-1">
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import { store, persistor } from 'redux/store';
-// import { BrowserRouter } from 'react-router-dom';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { ThemeProvider } from 'styled-components';
-
-// import { GlobalStyle } from 'styles/GlobalStyles';
-// import { App } from 'App';
-// import theme from 'styles/theme';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <ThemeProvider theme={theme}>
-//       <GlobalStyle />
-//       <BrowserRouter basename="/">
-//         <App />
-//       </BrowserRouter>
-//     </ThemeProvider>
-//   </React.StrictMode>
-// );
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <ThemeProvider theme={theme}>
-//       <GlobalStyle />
-//       <Provider store={store}>
-//         <BrowserRouter basename="/">
-//           <PersistGate loading={null} persistor={persistor}>
-//             <App />
-//           </PersistGate>
-//         </BrowserRouter>
-//       </Provider>
-//     </ThemeProvider>
-//   </React.StrictMode>
-// );
