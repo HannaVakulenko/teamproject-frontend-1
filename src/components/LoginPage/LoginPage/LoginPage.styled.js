b8f0246daf6da6b8516e7d46ac6023a0e4238575
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-08-11 17:24:38 +0300

    Changed format for bg image to webp

--- src/components/LoginPage/LoginPage/LoginPage.styled.js
+++ src/components/LoginPage/LoginPage/LoginPage.styled.js
@@ -1,7 +1,8 @@
 import { styled } from "styled-components";
 import { device } from "constants";
-import gooseImage from "assets/images/loginform-desk-1x.png";
-import gooseImageRetina from "assets/images/loginform-desk@2x.png";
+import gooseImage1x from "assets/images/loginform-desk-1x.webp";
+import gooseImage2x from "assets/images/loginform-desk@2x.webp";
+import gooseImage3x from "assets/images/loginform-desk@3x.webp";
 
 export const PageContainer = styled.div`
   height: 100vh;
@@ -24,11 +25,16 @@ export const BgImage = styled.div`
     display: block;
     width: 368px;
     height: 521px;
-    background-image: url(${gooseImage});
+    background-image: url(${gooseImage1x});
 
     @media (min-device-pixel-ratio: 2), (min-resolution: 192dpi), (min-resolution: 2dppx) {
-      background-image: url(${gooseImageRetina});
+      background-image: url(${gooseImage2x});
       background-size: contain;
-    }
+    };
+
+    @media (min-device-pixel-ratio: 3), (min-resolution: 300dpi), (min-resolution: 3dppx) {
+      background-image: url(${gooseImage3x});
+      background-size: contain;
+    };
   };
 `;
--- src/components/RegisterPage/RegisterPage/RegisterPage.styled.js
+++ src/components/RegisterPage/RegisterPage/RegisterPage.styled.js
@@ -1,7 +1,8 @@
 import { styled } from "styled-components";
 import { device } from "constants";
-import gooseImage from "assets/images/registerform-desk-1x.png";
-import gooseImageRetina from "assets/images/registerform-desk@2x.png";
+import gooseImage1x from "assets/images/registerform-desk-1x.webp";
+import gooseImage2x from "assets/images/registerform-desk@2x.webp";
+import gooseImage3x from "assets/images/registerform-desk@3x.webp";
 
 export const PageContainer = styled.div`
   height: 100vh;
@@ -24,11 +25,16 @@ export const BgImage = styled.div`
     display: block;
     width: 400px;
     height: 416px;
-    background-image: url(${gooseImage});
+    background-image: url(${gooseImage1x});
 
     @media (min-device-pixel-ratio: 2), (min-resolution: 192dpi), (min-resolution: 2dppx) {
-      background-image: url(${gooseImageRetina});
+      background-image: url(${gooseImage2x});
       background-size: contain;
-    }
+    };
+
+    @media (min-device-pixel-ratio: 3), (min-resolution: 300dpi), (min-resolution: 3dppx) {
+      background-image: url(${gooseImage3x});
+      background-size: contain;
+    };
   };
 `;
