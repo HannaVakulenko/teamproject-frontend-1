b1a1355a6a676ffbd2441aec33cc35843224d5ea
Aleksandr Lupu <alexandr.lupu.1984@gmail.com>
2023-08-11 17:28:35 +0300

    update theme.js

--- src/components/MainLayout/ThemeToggler/ThemeToggler.js
+++ src/components/MainLayout/ThemeToggler/ThemeToggler.js
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react';
+import { ReactComponent as Icons } from '../../../assets/icons/symbol-defs.svg';
+
 const ThemeToggler = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
-    <button type="button">O</button>
+    <button type="button" onClick={toggleTheme}>
+      <svg width="24" height="24">
+        <use href={Icons + (theme === 'light' ? '#icon-sun' : '#icon-moon')} />
+        {/* <use href={`#${theme === 'light' ? 'icon-sun' : 'icon-moon'}`} /> */}
+      </svg>
+    </button>
   );
 };
 export default ThemeToggler;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -106,31 +106,29 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     line-height: 1.4;
-    color: #333;
-    background-color: #fff;
+    color: var(--colors-text);
+    background-color: var(--colors-bg);
   }
 
   body[data-theme='light'] {
+    --colors-bg: #ffffff
     --colors-text: #111111
     --colors-calendar-text: #343434
     --colors-btn: #3E85F3
     --colors-active: #E3F3FF
-    --colors-task-bg: linear-gradient(0deg, rgba(220, 227, 229, 0.8), rgba(220, 227, 229, 0.8)),
-linear-gradient(0deg, #F7F6F9, #F7F6F9);
---colors-tasks-bg: linear-gradient(0deg, rgba(220, 227, 229, 0.8), rgba(220, 227, 229, 0.8)),
-linear-gradient(0deg, #FFFFFF, #FFFFFF);
+    --colors-task-bg: linear-gradient(0deg, rgba(220, 227, 229, 0.8), rgba(220, 227, 229, 0.8)), linear-gradient(0deg, #F7F6F9, #F7F6F9);
+    --colors-tasks-bg: linear-gradient(0deg, rgba(220, 227, 229, 0.8), rgba(220, 227, 229, 0.8)), linear-gradient(0deg, #FFFFFF, #FFFFFF);
 
 
   }
 
   body[data-theme='dark'] {
+    --colors-bg: #13151A
     --colors-text: #FFFFFF
     --colors-btn: #3E85F3
     --colors-active: #3E85F3
-    --colors-task-bg: linear-gradient(0deg, #171820, #171820),
-linear-gradient(0deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15));
---colors-tasks-bg: linear-gradient(0deg, #21222C, #21222C),
-linear-gradient(0deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15));
+    --colors-task-bg: linear-gradient(0deg, #171820, #171820), linear-gradient(0deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15));
+    --colors-tasks-bg: linear-gradient(0deg, #21222C, #21222C), linear-gradient(0deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15));
 
 
   }
