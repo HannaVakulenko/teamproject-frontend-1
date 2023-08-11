75c772f54f6aaa0f6877fd0e3880db526d627bf1
Sergey Donyk <doncarlos@i.ua>
2023-08-12 02:48:18 +0300

    dayInGrid

--- src/components/CalendarPage/DatePicker/DatePicker.js
+++ src/components/CalendarPage/DatePicker/DatePicker.js
@@ -1,12 +1,56 @@
+// import React from 'react';
+// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+// import { CalendarContainer, StyledDatePicker } from './DatePiker.syled';
+
+// const MyDatePicker = () => {
+//   return (
+//     <CalendarContainer>
+//       <div className="weekdays">
+//         <div>Sun</div>
+//         <div>Mon</div>
+//         <div>Tue</div>
+//         <div>Wed</div>
+//         <div>Thu</div>
+//         <div>Fri</div>
+//         <div>Sat</div>
+//       </div>
+//       <StyledDatePicker
+//         dateAdapter={AdapterDateFns}
+//         openTo="day"
+//         views={['day']}
+//         disableToolbar
+//       />
+//     </CalendarContainer>
+//   );
+// };
+
+// export default MyDatePicker;
 import React from 'react';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { CalendarWrapper } from './DatePiker.syled';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import {
+  CalendarContainer,
+  StyledDatePicker,
+  WeekdayCell,
+} from './DatePiker.syled';
 
 const MyDatePicker = () => {
   return (
-    <CalendarWrapper>
-      <DatePicker />
-    </CalendarWrapper>
+    <CalendarContainer>
+      <WeekdayCell>Sun</WeekdayCell>
+      <WeekdayCell>Mon</WeekdayCell>
+      <WeekdayCell>Tue</WeekdayCell>
+      <WeekdayCell>Wed</WeekdayCell>
+      <WeekdayCell>Thu</WeekdayCell>
+      <WeekdayCell>Fri</WeekdayCell>
+      <WeekdayCell>Sat</WeekdayCell>
+      <StyledDatePicker
+        dateAdapter={AdapterDateFns}
+        openTo="day"
+        views={['day']}
+        disableToolbar
+      />
+    </CalendarContainer>
   );
 };
+
 export default MyDatePicker;
--- src/components/CalendarPage/DatePicker/DatePiker.syled.js
+++ src/components/CalendarPage/DatePicker/DatePiker.syled.js
@@ -1,8 +1,76 @@
+// import styled from 'styled-components';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+// export const CalendarContainer = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(7, 1fr);
+//   gap: 2px;
+// `;
+
+// export const StyledDatePicker = styled(DatePicker)`
+//   .MuiPickersDay-root {
+//     width: 100%;
+//     height: 50px;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     border: 1px solid #ccc; /* Додайте рамку для кожного дня */
+//     box-sizing: border-box; /* Збереження розміру включаючи рамку */
+//   }
+
+//   .MuiPickersCalendarHeader-switchHeader {
+//     display: none;
+//   }
+
+//   .MuiPickersCalendarHeader-daysHeader {
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     padding: 8px;
+//   }
+
+//   .MuiPickersCalendarHeader-iconButton {
+//     display: none;
+//   }
+// `;
 import styled from 'styled-components';
 
-export const CalendarWrapper = styled.div`
+export const CalendarContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  gap: 2px;
+`;
+
+export const WeekdayCell = styled.div`
+  width: 100%;
+  height: 50px;
   display: flex;
-  justify-content: center;
   align-items: center;
-  height: 80vh; /* Змініть висоту за потребою */
+  justify-content: center;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+`;
+
+export const StyledDatePicker = styled(DatePicker)`
+  .MuiPickersDay-root {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+
+  .MuiPickersCalendarHeader-switchHeader {
+    display: none;
+  }
+
+  .MuiPickersCalendarHeader-daysHeader {
+    display: none;
+  }
+
+  .MuiPickersCalendarHeader-iconButton {
+    display: none;
+  }
 `;
