46d110bb0d796d2fb23630091602f22585435b21
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-08-12 18:30:45 +0300

    add feedback modal

--- src/components/MainLayout/AddFeedbackModal/AddFeedbackModal.js
+++ src/components/MainLayout/AddFeedbackModal/AddFeedbackModal.js
@@ -1,8 +1,50 @@
-const AddFeedbackModal = () => {
-  return (
-    <>
-      <div></div>
-    </>
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { ModalDiv, Overlay } from './AddFeedbackModal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const AddFeedbackModal = ({ closeModal, imgUrl, tag }) => {
+  useEffect(() => {
+    const onKeyDown = e => {
+      // перевірка клавіші Escape
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    };
+    // слухач для кнопок
+    window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      // чистимо за собою після закриття модалки
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  });
+
+  // console.log(children);
+  // закриття модалки по кліку на бекдроп
+  const onBackdropeClick = e => {
+    // перевірка чи клік був на бекдроп
+    if (e.currentTarget === e.target) {
+      closeModal();
+    }
+  };
+
+  return createPortal(
+    <Overlay onClick={onBackdropeClick}>
+      <ModalDiv>
+        <div>AddFeedbackModal</div>
+      </ModalDiv>
+    </Overlay>,
+    modalRoot
   );
 };
+
 export default AddFeedbackModal;
+
+AddFeedbackModal.propTypes = {
+  imgUrl: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  closeModal: PropTypes.func.isRequired,
+};
