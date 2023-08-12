5e922485bca493d4e12cca917e2ae6266f2a423f
Aleksandr Lupu <alexandr.lupu.1984@gmail.com>
2023-08-12 07:03:02 +0300

    update thema.js

--- src/components/MainLayout/ThemeToggler/ThemeToggler.js
+++ src/components/MainLayout/ThemeToggler/ThemeToggler.js
@@ -1,25 +1,44 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+// import { ReactComponent as Icons } from '../../../assets/icons/symbol-defs.svg';
+// import { lightTheme, darkTheme } from '../../../styles/theme';
+
+// const ThemeToggler = () => {
+//   const [isLightkTheme, setIsLightTheme] = useState(false);
+
+//   const toggleTheme = () => {
+//     setIsLightTheme(prevIsLightTheme => !prevIsLightTheme);
+//   };
+
+//   return (
+//     <button type="button" onClick={toggleTheme}>
+//       <svg width="24" height="24">
+//         <use
+//           href={
+//             Icons +
+//             (isLightkTheme === 'lightTheme' ? '#icon-sun' : '#icon-moon')
+//           }
+//         />
+//       </svg>
+//     </button>
+//   );
+// };
+// export default ThemeToggler;
+
+import React from 'react';
 import { ReactComponent as Icons } from '../../../assets/icons/symbol-defs.svg';
-import { lightTheme, darkTheme } from '../../../styles/theme';
 
-const ThemeToggler = () => {
-  const [isLightkTheme, setIsLightTheme] = useState(false);
-
-  const toggleTheme = () => {
-    setIsLightTheme(prevIsLightTheme => !prevIsLightTheme);
-  };
-
+const ThemeToggler = ({ toggleTheme, isLightTheme }) => {
   return (
     <button type="button" onClick={toggleTheme}>
       <svg width="24" height="24">
         <use
           href={
-            Icons +
-            (isLightkTheme === 'lightTheme' ? '#icon-sun' : '#icon-moon')
+            Icons + (isLightTheme ? '#icon-sun' : '#icon-moon') // Використання переданого стану
           }
         />
       </svg>
     </button>
   );
 };
+
 export default ThemeToggler;
