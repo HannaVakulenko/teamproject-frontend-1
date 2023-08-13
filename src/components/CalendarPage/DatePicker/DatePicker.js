7c0160e7f7d4ae0797077a0f24524908f9d31655
Sergey Donyk <doncarlos@i.ua>
2023-08-14 00:49:29 +0300

    startWeekMon

--- src/components/CalendarPage/DatePicker/DatePicker.js
+++ src/components/CalendarPage/DatePicker/DatePicker.js
@@ -1,83 +1,23 @@
-import React from 'react';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import en from 'date-fns/locale/en-US';
 
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import {
-  CalendarContainer,
-  StyledDatePicker,
-  WeekdayCell,
-} from './DatePiker.styled';
-
-const MyDatePicker = () => {
-  const locale = {
-    weekStartsOn: 1,
-  };
+registerLocale('en', en);
+const MyDatePicker = props => {
   return (
-    <CalendarContainer>
-      <WeekdayCell>Mon</WeekdayCell>
-      <WeekdayCell>Tue</WeekdayCell>
-      <WeekdayCell>Wed</WeekdayCell>
-      <WeekdayCell>Thu</WeekdayCell>
-      <WeekdayCell>Fri</WeekdayCell>
-      <WeekdayCell>Sat</WeekdayCell>
-      <WeekdayCell>Sun</WeekdayCell>
-      <StyledDatePicker
-        dateAdapter={AdapterDateFns}
-        open
-        views={['day']}
-        disableToolbar
-        firstDayOfWeek={1}
-        locale={locale}
+    <div>
+      <DatePicker
+        selected={props.date}
+        onChange={props.onChange}
+        dropdownMode="select"
+        dateFormat="yyyy.MM.dd."
+        todayButton="Ma"
+        closeOnScroll={true}
+        locale="en"
+        calendarStartDay={1}
       />
-    </CalendarContainer>
+    </div>
   );
 };
 
 export default MyDatePicker;
-// // <DatePicker
-// //   label="Select a date"
-// //   value={selectedDate}
-// //   onChange={date => setSelectedDate(date)}
-// //   renderInput={params => <TextField {...params} />}
-// //   startDay="1" // Monday
-// //   shouldDisableDate={() => false}
-// // />;
-// import React, { useState } from 'react';
-// import { DatePicker } from '@mui/x-date-pickers';
-// import { TextField } from '@mui/material';
-// import { LocalizationProvider } from '@mui/x-date-pickers';
-// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-
-// const MyDatePicker = () => {
-//   const [selectedDate, setSelectedDate] = useState(null);
-
-//   return (
-//     <div>
-//       <LocalizationProvider dateAdapter={AdapterDateFns}>
-//         <DatePicker
-//           label="Select a date"
-//           value={selectedDate}
-//           onChange={date => setSelectedDate(date)}
-//           renderInput={params => <TextField {...params} />}
-//           startDay="1" // Monday
-//           shouldDisableDate={() => false}
-//         />
-//       </LocalizationProvider>
-//     </div>
-//   );
-// };
-
-// export default MyDatePicker;
-
-// import AdapterDateFns from '@mui/lab/AdapterDateFns';
-// import LocalizationProvider from '@mui/lab/LocalizationProvider';
-
-//  <LocalizationProvider dateAdapter={AdapterDateFns}>
-//    <DatePicker
-//      label="Select a date"
-//      value={selectedDate}
-//      onChange={date => setSelectedDate(date)}
-//      renderInput={params => <TextField {...params} />}
-//      startDay="1" // Monday
-//      shouldDisableDate={() => false}
-//    />
-//  </LocalizationProvider>;
--- src/components/CalendarPage/DatePicker/DatePiker.styled.js
+++ src/components/CalendarPage/DatePicker/DatePiker.styled.js
@@ -1,44 +1,3 @@
-// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
-// import styled from 'styled-components';
-
-// export const CalendarContainer = styled.div`
-//   border: 1px solid #ccc;
-//   box-sizing: border-box;
-//   display: flex;
-//   // flex-direction: column;
-//   align-items: center;
-// `;
-
-// export const WeekdayCell = styled.div`
-//   width: 100%;
-//   height: 50px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `;
-
-// export const StyledDatePicker = styled(DatePicker)`
-//   .MuiPickersDay-root {
-//     width: 100%;
-//     height: 50px;
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//   }
-
-//   .MuiPickersCalendarHeader-switchHeader {
-//     display: none;
-//   }
-
-//   .MuiPickersCalendarHeader-daysHeader {
-//     display: none;
-//   }
-
-//   .MuiPickersCalendarHeader-iconButton {
-//     display: none;
-//   }
-// `;
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import styled from 'styled-components';
 
@@ -56,13 +15,9 @@ export const CalendarContainer = styled.div`
   }
 
   @media (min-width: 376px) and (max-width: 768px) {
-    /* Стилі для планшетної версії (ширина від 376px до 768px) */
-    /* Можна додати додаткові стилі для планшетної версії */
   }
 
   @media (min-width: 769px) and (max-width: 1440px) {
-    /* Стилі для настільної версії (ширина від 769px до 1440px) */
-    /* Можна додати додаткові стилі для настільної версії */
   }
 `;
 
@@ -95,3 +50,44 @@ export const StyledDatePicker = styled(DatePicker)`
     display: none;
   }
 `;
+
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
+// import styled from 'styled-components';
+
+// export const CalendarContainer = styled.div`
+//   border: 1px solid #ccc;
+//   box-sizing: border-box;
+//   display: grid;
+//   grid-template-columns: repeat(7, 1fr);
+//   gap: 2px;
+//   width: 100%;
+
+//   @media (max-width: 375px) {
+//     width: 335px;
+//     display: flex;
+//   }
+// `;
+
+// export const WeekdayCell = styled.div`
+//   width: 100%;
+//   height: 50px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
+
+// export const StyledDatePicker = styled(DatePicker)`
+//   .react-datepicker__day {
+//     width: 100%;
+//     height: 50px;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//   }
+
+//   .react-datepicker__navigation--previous,
+//   .react-datepicker__navigation--next {
+//     display: none;
+//   }
+// `;
