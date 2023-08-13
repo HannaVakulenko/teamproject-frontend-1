42b8c0c32f7251dc6e079c294b69e32996ab8c06
Oleh Protasevych <protasevicholeg@gmail.com>
2023-08-13 23:34:35 +0300

    some MainLayout style fixes

--- src/pages/CalendarPage.js
+++ src/pages/CalendarPage.js
@@ -2,15 +2,13 @@ import { Section } from 'components/Common';
 
 const CalendarPage = () => (
   <>
-    <div>
-      <Section>
-        <h2>CalendarPage</h2>
-        <p>
-          dfuhdkfhgfkdfjhgkjdfgdfkjghfdkjghkjdfhgkjfd
-          dfkghdfkjghkdfjhgkjdfhgkjdf gdfjghdfkjhgkjdfhgkjfdhng
-        </p>
-      </Section>
-    </div>
+    <Section>
+      <h2>CalendarPage</h2>
+      <p>
+        dfuhdkfhgfkdfjhgkjdfgdfkjghfdkjghkjdfhgkjfd dfkghdfkjghkdfjhgkjdfhgkjdf
+        gdfjghdfkjhgkjdfhgkjfdhng
+      </p>
+    </Section>
   </>
 );
 
