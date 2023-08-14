16d3db9ce50fbded9ab16d13d82883a5017ffa12
Oleh Protasevych <protasevicholeg@gmail.com>
2023-08-14 21:05:05 +0300

    fix own

--- src/redux/reviews/operations.js
+++ src/redux/reviews/operations.js
@@ -1,20 +1,20 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 axios.defaults.baseURL = 'https://goose-track-gr25.onrender.com';
 
 export const fetchReviews = createAsyncThunk(
-    'reviews/getREviews',
-    async ({ page, limit }, thunkAPI) => {
-        try {
-            const { data } = await axios.get(`/api/reviews?page=${page}&limit=${limit}`);
-            return data;
-        
-        } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
-        }
-
+  'reviews/getREviews',
+  async ({ page, limit }, thunkAPI) => {
+    try {
+      const { data } = await axios.get(
+        `/api/reviews?page=${page}&limit=${limit}`
+      );
+      return data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
+  }
 );
 
 export const addReview = createAsyncThunk(
@@ -24,45 +24,45 @@ export const addReview = createAsyncThunk(
       const { data } = await axios.post('/api/reviews', review);
       return data;
     } catch (e) {
-      return  thunkAPI.rejectWithValue(e.message);
+      return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
 
 export const deleteReview = createAsyncThunk(
   'reviews/deleteReviews',
-  async (id, thunkAPI) => {
+  async (_, thunkAPI) => {
     try {
-      const { data } = await axios.delete(`/api/reviews/${id}`);
+      const { data } = await axios.delete(`/api/reviews/own`);
 
       return data;
     } catch (e) {
-        thunkAPI.rejectWithValue(e.message);
+      thunkAPI.rejectWithValue(e.message);
     }
   }
 );
 
 export const editReview = createAsyncThunk(
-    'reviews/editReviews',
-    async ({id, review}, thunkAPI) => {
-      try {
-        const { data } = await axios.patch(`/api/reviews/${id}`, review);
-  
-        return data;
-      } catch (e) {
-          thunkAPI.rejectWithValue(e.message);
-      }
-    }
-  );
-
-export const fetchReviewById = createAsyncThunk(
-  'reviews/fetchReviewById',
-  async (id, thunkAPI) => {
+  'reviews/editReviews',
+  async ({ _, review }, thunkAPI) => {
     try {
-      const { data } = await axios.get(`/api/reviews/${id}`);
+      const { data } = await axios.patch(`/api/reviews/own`, review);
+
       return data;
     } catch (e) {
-        thunkAPI.rejectWithValue(e.message);
+      thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const fetchReviewOwn = createAsyncThunk(
+  'reviews/fetchReviewById',
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axios.get(`/api/reviews/own`);
+      return data;
+    } catch (e) {
+      thunkAPI.rejectWithValue(e.message);
     }
   }
 );
