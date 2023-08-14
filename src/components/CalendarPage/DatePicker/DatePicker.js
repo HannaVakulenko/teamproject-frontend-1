bdb46703c4b65ebb70768effe9f32ce33f313b62
Sergey Donyk <doncarlos@i.ua>
2023-08-14 04:31:40 +0300

    upDayNames

--- src/components/CalendarPage/DatePicker/DatePicker.js
+++ src/components/CalendarPage/DatePicker/DatePicker.js
@@ -1,14 +1,93 @@
+// import DatePicker, { registerLocale } from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
+// import en from 'date-fns/locale/en-US';
+// import { DayNamesWrapper, DayName } from './DatePiker.styled';
+
+// registerLocale('en', en);
+// const MyDatePicker = props => {
+//   return (
+//     <div>
+//       <DayNamesWrapper>
+//         <DayName>Sun</DayName>
+//         <DayName>Mon</DayName>
+//         <DayName>Tue</DayName>
+//         <DayName>Wed</DayName>
+//         <DayName>Thu</DayName>
+//         <DayName>Fri</DayName>
+//         <DayName>Sat</DayName>
+//       </DayNamesWrapper>
+//       <DatePicker
+//         selected={props.date}
+//         onChange={props.onChange}
+//         open
+//         dropdownMode="select"
+//         dateFormat="yyyy.MM.dd."
+//         todayButton="Ma"
+//         closeOnScroll={true}
+//         locale="en"
+//         calendarStartDay={1}
+//       />
+//     </div>
+//   );
+// };
+
+// export default MyDatePicker;
+
+//react-style-css
+// import React from 'react';
+// import DatePicker, { registerLocale } from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker.css';
+// import en from 'date-fns/locale/en-US';
+// import styled from 'styled-components';
+// import { datePickerStyles } from './DatePiker.styled';
+
+// registerLocale('en', en);
+
+// const CustomDatePicker = styled(DatePicker)`
+//   /* Стилі вашого календаря */
+//   ${datePickerStyles};
+// `;
+
+// const MyDatePicker = props => {
+//   return (
+//     <div>
+//       <CustomDatePicker
+//         selected={props.date}
+//         onChange={props.onChange}
+//         open
+//         dropdownMode="select"
+//         dateFormat="yyyy.MM.dd."
+//         todayButton="Ma"
+//         closeOnScroll={true}
+//         locale="en"
+//         calendarStartDay={1}
+//       />
+//     </div>
+//   );
+// };
+
+// export default MyDatePicker;
+import React from 'react';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import en from 'date-fns/locale/en-US';
+import styled from 'styled-components';
+import { GlobalStyles, CustomDatePickerStyles } from './DatePiker.styled';
 
 registerLocale('en', en);
+
+const CustomDatePicker = styled(DatePicker)`
+  ${CustomDatePickerStyles}
+`;
+
 const MyDatePicker = props => {
   return (
     <div>
-      <DatePicker
+      <GlobalStyles /> {/* Застосовуємо глобальні стилі */}
+      <CustomDatePicker
         selected={props.date}
         onChange={props.onChange}
+        open
         dropdownMode="select"
         dateFormat="yyyy.MM.dd."
         todayButton="Ma"
@@ -21,3 +100,31 @@ const MyDatePicker = props => {
 };
 
 export default MyDatePicker;
+
+// import { css } from 'styled-components';
+
+// export const datePickerStyles = css`
+//   /* Стилі для react-datepicker__day-names */
+//   .react-datepicker__day-names {
+//         display: none;
+//     position: absolute;
+//     top: -40px
+//     left: 0;
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     background-color: #f5f5f5;
+//     padding: 8px;
+//     border-top-left-radius: 4px;
+//     border-top-right-radius: 4px;
+//     width: 100%; /* Розтягуємо на всю ширину */
+//     z-index: 1; /* Забезпечуємо відображення над календарем */
+//   }
+
+//   .react-datepicker__day-name {
+//     font-weight: bold;
+//     text-align: center;
+//     flex-basis: calc(100% / 7);
+//   }
+// `;
+//Стилі для react-datepicker__day-names */
--- src/components/CalendarPage/DatePicker/DatePiker.styled.js
+++ src/components/CalendarPage/DatePicker/DatePiker.styled.js
@@ -1,93 +1,25 @@
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
-export const CalendarContainer = styled.div`
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  gap: 2px;
-  width: 100%;
-
-  @media (max-width: 375px) {
-    width: 335;
+export const GlobalStyles = createGlobalStyle`
+  /* Тут ви можете задати ваші глобальні стилі */
+  .react-datepicker__day-names {
     display: flex;
+    top: -40px;
+    position: absolute;
+    
+    .react-datepicker__input-container {
+display:none;
+    
   }
+  .react-datepicker-wrapper{
+    width:100%;
 
-  @media (min-width: 376px) and (max-width: 768px) {
-  }
-
-  @media (min-width: 769px) and (max-width: 1440px) {
-  }
-`;
-
-export const WeekdayCell = styled.div`
-  width: 100%;
-  height: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const StyledDatePicker = styled(DatePicker)`
-  .MuiPickersDay-root {
-    width: 100%;
-    height: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .MuiPickersCalendarHeader-switchHeader {
-    display: none;
-  }
-
-  .MuiPickersCalendarHeader-daysHeader {
-    display: none;
-  }
-
-  .MuiPickersCalendarHeader-iconButton {
-    display: none;
   }
 `;
 
-// import DatePicker from 'react-datepicker';
-// import 'react-datepicker/dist/react-datepicker.css';
-// import styled from 'styled-components';
+// Інші стилі для календаря
+export const CustomDatePickerStyles = `
+  /* Стилі для календаря, які перебивають стилі бібліотеки */
 
-// export const CalendarContainer = styled.div`
-//   border: 1px solid #ccc;
-//   box-sizing: border-box;
-//   display: grid;
-//   grid-template-columns: repeat(7, 1fr);
-//   gap: 2px;
-//   width: 100%;
-
-//   @media (max-width: 375px) {
-//     width: 335px;
-//     display: flex;
-//   }
-// `;
-
-// export const WeekdayCell = styled.div`
-//   width: 100%;
-//   height: 50px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `;
-
-// export const StyledDatePicker = styled(DatePicker)`
-//   .react-datepicker__day {
-//     width: 100%;
-//     height: 50px;
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//   }
-
-//   .react-datepicker__navigation--previous,
-//   .react-datepicker__navigation--next {
-//     display: none;
-//   }
-// `;
+  }
+`;
