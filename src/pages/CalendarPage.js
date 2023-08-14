c9b2772958e779d66850cf9acd33c41c2f9dc94d
Sergey Donyk <doncarlos@i.ua>
2023-08-14 22:19:59 +0300

    newst

--- src/pages/CalendarPage.js
+++ src/pages/CalendarPage.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import moment from 'moment';
-import
-CalendarGrid,
-CalendarToolbar,
-PeriodPaginator,
-'components/CalendarPage';
+import { CalendarGrid, CalendarToolbar } from 'components/CalendarPage';
 // import MyDatePicker from 'components/CalendarPage/DatePicker/DatePicker';
 const startDay = moment().startOf('month').startOf('week');
 const CalendarPage = () => {
   return (
     <div>
-       <CalendarToolbar /> 
-      <PeriodPaginator /> 
+      <CalendarToolbar />
+      {/* <PeriodPaginator />  */}
       <CalendarGrid startDay={startDay} />
-     
     </div>
   );
 };
