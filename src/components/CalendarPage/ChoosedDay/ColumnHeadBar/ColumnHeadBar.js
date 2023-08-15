dfad9882d8566795de2144367ca423d2b706dad6
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-08-15 22:37:28 +0300

    Updated styles for ColumnHeadBar component

--- src/components/CalendarPage/ChoosedDay/ColumnHeadBar/ColumnHeadBar.js
+++ src/components/CalendarPage/ChoosedDay/ColumnHeadBar/ColumnHeadBar.js
@@ -1,10 +1,10 @@
-import { Button, Icon, Wrapper } from "./ColumnHeadBar.styled";
+import { Button, Icon, Title, Wrapper } from "./ColumnHeadBar.styled";
 import icon from "assets/icons/symbol-defs.svg";
 
 const ColumnHeadBar = ({ title }) => {
   return (
     <Wrapper>
-      <h3>{title}</h3>
+      <Title>{title}</Title>
       <Button aria-label="Add new task">
         <Icon width="22" height="22">
           <use href={icon + "#icon-plus-in-a-circle"}></use>
--- src/components/CalendarPage/ChoosedDay/ColumnHeadBar/ColumnHeadBar.styled.js
+++ src/components/CalendarPage/ChoosedDay/ColumnHeadBar/ColumnHeadBar.styled.js
@@ -1,10 +1,23 @@
 import { styled } from 'styled-components';
+import { device } from 'constants';
 
 export const Wrapper = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
+`;
+
+export const Title = styled.h3`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 1.11;
+  color: #111;
+
+  @media screen and (min-width: ${device.tablet}px) {
+    font-size: 20px;
+    line-height: 1.2;
+  }
 `;
 
 export const Button = styled.button`
