54ad9e84b045f13923b8c18a7a570442971dd742
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-08-15 14:59:09 +0200

    додала yup схему

--- src/components/Common/TaskForm/TaskForm.js
+++ src/components/Common/TaskForm/TaskForm.js
@@ -1,14 +1,59 @@
-// import { Formik } from 'formik';
-// import * as Yup from 'yup';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+// import { useParams } from 'react-router-dom';
+// import { useDispatch } from 'react-redux';
+// import icon from 'assets/icons/symbol-defs.svg';
 
-
+const TaskSchema = Yup.object().shape({
+  title: Yup.string()
+    .max(250, 'Title is too long')
+    .required('Title is required'),
+  start: Yup.string().required('Start time is required'),
+  end: Yup.string()
+    .required('End time is required')
+    .test(
+      'is-greater',
+      'End time should be greater than start time',
+      function (value) {
+        const { start } = this.parent;
+        if (start && value) {
+          const startTime = new Date(`2000-01-01T${start}`);
+          const endTime = new Date(`2000-01-01T${value}`);
+          return endTime > startTime;
+        }
+        return true;
+      }
+    ),
+  priority: Yup.string()
+    .oneOf(['low', 'medium', 'high'])
+    .required('Priority is required')
+    .transform((value, originalValue) => {
+      if (originalValue) {
+        const [year, month, day] = originalValue.split('-');
+        return new Date(
+          `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+        );
+      }
+      return value;
+    }),
+  category: Yup.string()
+    .oneOf(['to-do', 'in-progress', 'done'])
+    .required('Category is required'),
+});
 
 
 const TaskForm = () => {
+
+  //  const { _id, title, priority, start, end, category } = taskToEdit;
+
+  // const dispatch = useDispatch();
+  
+  
   return (
-    <>
-      <div></div>
-    </>
+    <Formik
+    validationSchema={TaskSchema}>
+      
+  </Formik>
   );
 };
 export default TaskForm;
