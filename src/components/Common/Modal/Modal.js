27801d2011a0e600336c16ca11f871deb8ffd7a0
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-08-15 15:12:28 +0200

    створила чере createPortal модалку

--- src/components/Common/Modal/Modal.js
+++ src/components/Common/Modal/Modal.js
@@ -1,8 +1,41 @@
-const Modal = () => {
-  return (
-    <>
-      <div></div>
-    </>
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+import { Overlay } from "./Modal.styles";
+
+
+
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    const closeModalByEsc = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', closeModalByEsc);
+  
+    return () => {
+      document.body.style.overflow = 'auto';
+      document.removeEventListener('keydown', closeModalByEsc);
+    };
+  }, [onClose]);
+  
+  const handleOverlayClick = e => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <Overlay onClick={handleOverlayClick}>
+      <Modal>{ children}</Modal>
+    </Overlay>,
+    modalRoot
   );
 };
 export default Modal;
---
+++ src/components/Common/Modal/Modal.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Overlay = styled.div`
+`;
+
+
+export const Modal = styled.div`
+`;
