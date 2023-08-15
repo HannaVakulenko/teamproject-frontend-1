9d6e149affb524a9fce234cd423e590ba8f4ae73
Oleh Protasevych <protasevicholeg@gmail.com>
2023-08-15 18:43:28 +0300

    positioning file input and svg

--- src/components/AccountPage/UserForm/UserForm.js
+++ src/components/AccountPage/UserForm/UserForm.js
@@ -5,7 +5,8 @@ import { Formik, Form } from 'formik';
 import * as yup from 'yup';
 import { refreshUser } from 'redux/auth/operations';
 import { selectUser } from 'redux/auth/selectors';
-// import icon from 'assets/icons/symbol-defs.svg';
+import icon from 'assets/icons/symbol-defs.svg';
+import { Icon } from './UserForm.styled';
 
 const validationSchema = yup.object().shape({
   userName: yup.string().max(16, 'Max 16 characters').min(2, 'Min 2 characters'),
@@ -44,67 +45,79 @@ const UserForm = () => {
   };
 
   return (
-    <Formik initialValues={localUserData} validationSchema={validationSchema} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={localUserData}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
       {({ dirty, isValid, isSubmitting }) => (
         <Container>
           <FormContainer>
-             <ImageContainer>
-            {localUserData.profilePicture ? (
-              <UserImage
-                src={localUserData.profilePicture}
-                alt="User Profile"
-              />
+            <ImageContainer>
+              {localUserData.profilePicture ? (
+                <UserImage
+                  src={localUserData.profilePicture}
+                  alt="User Profile"
+                />
               ) : (
-               <UserImage src={user.avatarURL}></UserImage>
-            )}
-            <Upload
+                <UserImage src={user.avatarURL}></UserImage>
+              )}
+              <Upload
                 type="file"
                 accept="image/*"
-                onChange={(e) => {
+                onChange={e => {
                   const file = e.target.files[0];
                   if (file) {
                     const reader = new FileReader();
-                    reader.onload = (event) => {
-                      setLocalUserData({ ...localUserData, profilePicture: event.target.result });
+                    reader.onload = event => {
+                      setLocalUserData({
+                        ...localUserData,
+                        profilePicture: event.target.result,
+                      });
                     };
                     reader.readAsDataURL(file);
                   }
                 }}
-                />
+              />
+              <Icon>
+                <use href={icon + '#icon-user-check-01'}></use>
+              </Icon>
             </ImageContainer>
             <UserName>Name</UserName>
             <UserStatus>User</UserStatus>
-          <Form >
-            <Label>User Name:</Label>
-            <Input type="text" name="userName" placeholder="User Name" />
-            <ErrorText name="userName" component="div" />
- 
-             <Label>Birthday:</Label>
-            <Input
-              type="date"
-              name="birthday"
-            />
+            <Form>
+              <Label>User Name:</Label>
+              <Input type="text" name="userName" placeholder="User Name" />
+              <ErrorText name="userName" component="div" />
+
+              <Label>Birthday:</Label>
+              <Input type="date" name="birthday" />
               <ErrorText name="birthday" component="div" />
-              
-            <Label>Email:</Label>
-            <Input type="email" name="email" placeholder="Email" />
-            <ErrorText name="email" component="div" />
 
-            <Label>Phone:</Label>
-            <Input type="text" name="phone" placeholder="+380971234567" />
-            <ErrorText name="phone" component="div" />
+              <Label>Email:</Label>
+              <Input type="email" name="email" placeholder="Email" />
+              <ErrorText name="email" component="div" />
 
-            <Label>Skype:</Label>
-            <Input type="text" name="skype" placeholder="Enter your Skype ID" />
-            <ErrorText name="skype" component="div" />
+              <Label>Phone:</Label>
+              <Input type="text" name="phone" placeholder="+380971234567" />
+              <ErrorText name="phone" component="div" />
+
+              <Label>Skype:</Label>
+              <Input
+                type="text"
+                name="skype"
+                placeholder="Enter your Skype ID"
+              />
+              <ErrorText name="skype" component="div" />
             </Form>
-             <SaveChangesBtn
+            <SaveChangesBtn
               type="submit"
-              disabled={!dirty || !isValid || isSubmitting || formSubmitted}>
+              disabled={!dirty || !isValid || isSubmitting || formSubmitted}
+            >
               Save changes
             </SaveChangesBtn>
           </FormContainer>
-          </Container>
+        </Container>
       )}
     </Formik>
   );
--- src/components/AccountPage/UserForm/UserForm.styled.js
+++ src/components/AccountPage/UserForm/UserForm.styled.js
@@ -1,30 +1,27 @@
+// src/components/AccountPage/UserForm/UserForm.styled.js
 import styled from 'styled-components';
 import { Field, ErrorMessage } from 'formik';
 
-
 export const Container = styled.div`
-
-width: 375px;
-height: 844px;
-display: flex;
+  width: 375px;
+  height: 844px;
+  display: flex;
   flex-direction: column;
-  align-items: center; 
-
+  align-items: center;
 `;
 
 export const FormContainer = styled.div`
-
-width: 335px;
-height: 663px;
-padding: 30px;
-border-radius: 16px;
+  width: 335px;
+  height: 663px;
+  padding: 30px;
+  border-radius: 16px;
   margin: 95px 20px 40px;
   border: 1px solid #ccc;
   display: flex;
   flex-direction: column;
-  align-items: center; 
-   position: relative;
-   justify-content: center;
+  align-items: center;
+  position: relative;
+  justify-content: center;
 `;
 
 export const Label = styled.label`
@@ -32,18 +29,18 @@ export const Label = styled.label`
 `;
 
 export const Input = styled(Field)`
-color: #111;
-font-size: 14px;
-font-weight: 600;
-border-radius: 8px;
-border: 1px solid rgba(17, 17, 17, 0.10);
+  color: #111;
+  font-size: 14px;
+  font-weight: 600;
+  border-radius: 8px;
+  border: 1px solid rgba(17, 17, 17, 0.1);
   width: 299px;
   height: 42px;
   padding: 8px;
   margin-bottom: 10px;
-   background-color: ${props => (props.hasContent ? 'transparent' : 'white')};
+  background-color: ${props => (props.hasContent ? 'transparent' : 'white')};
 
-   &::placeholder {
+  &::placeholder {
     font-weight: normal;
   }
 `;
@@ -55,13 +52,14 @@ export const ErrorText = styled(ErrorMessage)`
 `;
 
 export const ImageContainer = styled.div`
-   position: absolute;
+  position: absolute;
   top: -8%;
   left: 50%;
   transform: translateX(-50%);
   display: flex;
   flex-direction: column;
   align-items: center;
+  z-index: 1;
 `;
 
 export const UserImage = styled.img`
@@ -70,20 +68,47 @@ export const UserImage = styled.img`
   border-radius: 50%;
 `;
 
+// export const Upload = styled.input`
+//   position: absolute;
+//   bottom: -2%;
+//   left: 70%;
+//   transform: translateX(-50%);
+//   /* width: 20px; */
+//   /* height: 20px; */
+//   /* border-radius: 50%; */
+//   border: none;
+//   cursor: pointer;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: transparent;
+// `;
+
 export const Upload = styled.input`
-position: absolute;
+  opacity: 0;
+  position: absolute;
   bottom: -2%;
   left: 70%;
   transform: translateX(-50%);
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  border: none;
+  width: 30px;
+  height: 30px;
   cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  border-radius: 50%;
   background-color: transparent;
+  z-index: 3;
+`;
+
+export const Icon = styled.svg`
+  position: absolute;
+  bottom: -2%;
+  left: 70%;
+  transform: translateX(-50%);
+  width: 30px;
+  height: 30px;
+  fill: red;
+  background-color: red;
+  border-radius: 50%;
+  z-index: 2;
 `;
 
 // export const Icon = styled.svg`
@@ -113,23 +138,21 @@ export const PlaceholderContainer = styled.div`
   align-items: center;
 `;
 
-
 export const UserName = styled.h6`
-font-size: 14px;
-font-weight: 700;
-margin-top: 30px;
+  font-size: 14px;
+  font-weight: 700;
+  margin-top: 30px;
 `;
 
 export const UserStatus = styled.span`
-font-size: 12px;
-font-weight: 600;
-margin-bottom: 40px;
+  font-size: 12px;
+  font-weight: 600;
+  margin-bottom: 40px;
 `;
 
 export const SaveChangesBtn = styled.button`
-display: flex;
+  display: flex;
   justify-content: center;
   align-items: center;
-    margin-top: 20px;
-
-`
+  margin-top: 20px;
+`;
