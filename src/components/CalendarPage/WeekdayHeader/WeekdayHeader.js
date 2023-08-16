757384913b49e1c9d1b1d3911c62e83a1411c281
Sergey Donyk <doncarlos@i.ua>
2023-08-16 04:39:54 +0300

    weekHeaderOK

--- src/components/CalendarPage/WeekdayHeader/WeekdayHeader.js
+++ src/components/CalendarPage/WeekdayHeader/WeekdayHeader.js
@@ -3,29 +3,29 @@ import { LargeWeekday, WeekWrapper } from './WeekdayHeader.styled';
 import { useMediaQuery } from 'react-responsive';
 
 const WeekdayHeader = () => {
-  const isTabletOrDesktop = useMediaQuery({ minWidth: 768 });
+  const isTabletOrDesktop = useMediaQuery({ minWidth: 376 });
 
   return (
     <WeekWrapper>
-      <LargeWeekday isWeekend={false}>
+      <LargeWeekday $weekend={false}>
         {isTabletOrDesktop ? 'mon' : 'm'}
       </LargeWeekday>
-      <LargeWeekday isWeekend={false}>
+      <LargeWeekday $weekend={false}>
         {isTabletOrDesktop ? 'tue' : 't'}
       </LargeWeekday>
-      <LargeWeekday isWeekend={false}>
+      <LargeWeekday $weekend={false}>
         {isTabletOrDesktop ? 'wed' : 'w'}
       </LargeWeekday>
-      <LargeWeekday isWeekend={false}>
+      <LargeWeekday $weekend={false}>
         {isTabletOrDesktop ? 'thu' : 't'}
       </LargeWeekday>
-      <LargeWeekday isWeekend={false}>
+      <LargeWeekday $weekend={false}>
         {isTabletOrDesktop ? 'fri' : 'f'}
       </LargeWeekday>
-      <LargeWeekday isWeekend={true}>
+      <LargeWeekday $weekend={true}>
         {isTabletOrDesktop ? 'sat' : 's'}
       </LargeWeekday>
-      <LargeWeekday isWeekend={true}>
+      <LargeWeekday $weekend={true}>
         {isTabletOrDesktop ? 'sun' : 's'}
       </LargeWeekday>
     </WeekWrapper>
--- src/components/CalendarPage/WeekdayHeader/WeekdayHeader.styled.js
+++ src/components/CalendarPage/WeekdayHeader/WeekdayHeader.styled.js
@@ -1,43 +1,37 @@
 import styled from 'styled-components';
-
 export const LargeWeekday = styled.div`
-  color: ${props => (props.isWeekend ? '#3E85F3' : '#343434')};
-  font-family: Inter;
-  font-weight: 600;
-  line-height: 18px;
-  letter-spacing: 0em;
-  text-align: center;
-  // color: #3e85f3;
-  font-size: 18px;
-  text-transform: uppercase;
+  color: ${props => (props.$weekend ? '#3E85F3' : '#343434')};
+
   @media (max-width: 375px) {
     font-family: Inter;
-    font-weight: 600;
-    line-height: 18px;
-    letter-spacing: 0em;
-    text-align: center;
-    // color: #3e85f3;
-    font-size: 18px;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 14px;
     text-transform: uppercase;
   }
 
   @media (min-width: 376px) and (max-width: 768px) {
     font-family: Inter;
-    font-weight: 600;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
     line-height: 18px;
-    letter-spacing: 0em;
-    text-align: center;
-    // color: #3e85f3;
-    font-size: 18px;
     text-transform: uppercase;
   }
 
   @media (min-width: 769px) and (max-width: 1440px) {
+    font-family: Inter;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 18px;
+    text-transform: uppercase;
   }
 `;
 
 export const SmallWeekday = styled.div`
-  color: ${props => (props.isWeekend ? '#343434' : '#343434')};
+  color: ${props => (props.$weekend ? '#3E85F3' : '#343434')};
   font-family: Inter;
   font-size: 16px;
   font-weight: 600;
@@ -53,40 +47,39 @@ export const SmallWeekday = styled.div`
     line-height: 18px;
     letter-spacing: 0em;
     text-align: center;
-    // color: #343434;
     text-transform: uppercase;
   }
 
   @media (min-width: 376px) and (max-width: 768px) {
-    color: ${props => (props.isWeekend ? '#343434' : '#343434')};
     font-family: Inter;
     font-size: 16px;
     font-weight: 600;
     line-height: 18px;
     letter-spacing: 0em;
     text-align: center;
-    // color: #343434;
     text-transform: uppercase;
   }
 
   @media (min-width: 769px) and (max-width: 1440px) {
-    font-size: 18px;
+    font-family: Inter;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 18px;
+    text-transform: uppercase;
   }
 `;
 
-// WeekWrapper;
 export const WeekWrapper = styled.div`
-  display: flex;
-  justify-content: space-around;
-  border: 1px solid rgba(220, 227, 229, 0.5);
-  border-radius: 8px;
-  background-color: white;
   @media (max-width: 375px) {
     display: flex;
     justify-content: space-around;
     border: 1px solid rgba(220, 227, 229, 0.5);
     border-radius: 8px;
     background-color: white;
+    max-width: 335px;
+    padding: 16px;
+    margin-bottom: 14px;
   }
 
   @media (min-width: 376px) and (max-width: 768px) {
@@ -95,6 +88,12 @@ export const WeekWrapper = styled.div`
     border: 1px solid rgba(220, 227, 229, 0.5);
     border-radius: 8px;
     background-color: white;
+    max-width: 704px;
+    padding-top: 14px;
+    padding-bottom: 14px;
+    padding-left: 40px;
+    padding-right: 40px;
+    margin-bottom: 18px;
   }
 
   @media (min-width: 769px) and (max-width: 1440px) {
@@ -103,5 +102,11 @@ export const WeekWrapper = styled.div`
     border: 1px solid rgba(220, 227, 229, 0.5);
     border-radius: 8px;
     background-color: white;
+    max-width: 1087px;
+    padding-top: 14px;
+    padding-bottom: 14px;
+    padding-left: 60px;
+    padding-right: 60px;
+    margin-bottom: 15px;
   }
 `;
