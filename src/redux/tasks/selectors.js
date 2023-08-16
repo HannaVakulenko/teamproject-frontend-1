16d4b82d10350a03535bb517898b098a5621a51c
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-08-16 13:56:16 +0300

    Adds avatarURL to redux-store

--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,3 +1,4 @@
 export const selectTasks = state => state.tasks.tasks;
+export const selectAvatarURL = state => state.tasks.avatarURL;
 export const selectIsLoading = state => state.tasks.isLoading;
 export const selectError = state => state.tasks.error;
