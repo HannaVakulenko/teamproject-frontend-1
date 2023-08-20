7b8f86d8487d4072bf4898ffca3efd733078d7f7
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2023-08-20 18:05:46 +0300

    done

--- src/components/CalendarPage/ChoosedDay/ColumnHeadBar/ColumnHeadBar.js
+++ src/components/CalendarPage/ChoosedDay/ColumnHeadBar/ColumnHeadBar.js
@@ -1,16 +1,34 @@
-import { Button, Icon, Title, Wrapper } from "./ColumnHeadBar.styled";
-import icon from "assets/icons/symbol-defs.svg";
+import { Button, Icon, Title, Wrapper } from './ColumnHeadBar.styled';
+import icon from 'assets/icons/symbol-defs.svg';
+import { useState } from 'react';
+import TaskModal from '../../../Common/TaskModal/TaskModal';
 
 const ColumnHeadBar = ({ title, category }) => {
-  const formatedTitle = title.charAt(0).toUpperCase() + title.slice(1).replace(/-/g, ' ');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [action, setAction] = useState('add');
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    setAction('add');
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const formatedTitle =
+    title.charAt(0).toUpperCase() + title.slice(1).replace(/-/g, ' ');
   return (
     <Wrapper>
       <Title>{formatedTitle}</Title>
-      <Button aria-label="Add new task">
+      <Button aria-label="Add new task" onClick={openModal}>
         <Icon width="22" height="22">
-          <use href={icon + "#icon-plus-in-a-circle"}></use>
+          <use href={icon + '#icon-plus-in-a-circle'}></use>
         </Icon>
       </Button>
+      {isModalOpen && (
+        <TaskModal onClose={closeModal} action={action} column={title} />
+      )}
     </Wrapper>
   );
 };
