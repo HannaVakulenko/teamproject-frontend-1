f4a000271d9f7e16b7fc27e35047899a0cb3a74b
Sergey Donyk <doncarlos@i.ua>
2023-08-22 02:41:44 +0300

    sam

--- src/components/CalendarPage/CalendarToolbar/PeriodPaginator/PeriodPaginator.js
+++ src/components/CalendarPage/CalendarToolbar/PeriodPaginator/PeriodPaginator.js
@@ -1,3 +1,91 @@
+// import {
+//   ButtonIncrease,
+//   ButtonDecrease,
+//   IconLeft,
+//   IconRight,
+//   DatePickerWrapperStyles,
+//   Date,
+//   ForDatePicker,
+//   Container,
+//   DatePickerWrapper,
+// } from './PeriodPaginator.styled';
+// import DatePicker, { registerLocale } from 'react-datepicker';
+// import icon from 'assets/icons/symbol-defs.svg';
+// import { useLocation, useNavigate } from 'react-router-dom';
+// import 'react-datepicker/dist/react-datepicker.css';
+// import { enGB } from 'date-fns/locale';
+// import { format } from 'date-fns';
+
+// registerLocale('enGB', enGB);
+
+// const PeriodPaginator = ({ date, getTasks, isOpen, setIsOpen, setDate }) => {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+
+//   const handleChange = e => {
+//     const formattedDate = format(e, 'yyyy-MM-dd');
+
+//     setIsOpen(!isOpen);
+//     setDate(e);
+
+//     if (location.pathname.startsWith('/calendar/month')) {
+//       navigate(`/calendar/month/${formattedDate}`);
+//     } else if (location.pathname.startsWith('/statistics')) {
+//       navigate(`/statistics/${formattedDate}`);
+//     } else {
+//       navigate(`/calendar/day/${formattedDate}`);
+//     }
+//   };
+//   const handleClick = e => {
+//     e.preventDefault();
+//     setIsOpen(!isOpen);
+//   };
+
+//   // console.log(date);
+
+//   return (
+//     <>
+//       <ForDatePicker>
+//         <Date className="example-custom-input" onClick={handleClick}>
+//           {location.pathname.startsWith('/calendar/day') ||
+//           location.pathname.startsWith('/statistics')
+//             ? format(date, 'dd MMM yyyy')
+//             : format(date, 'MMMM yyyy')}
+//         </Date>
+//         {isOpen && (
+//           <DatePickerWrapper>
+//             <DatePicker
+//               locale="enGB"
+//               onClickOutside={() => setIsOpen(!isOpen)}
+//               formatWeekDay={nameOfDay => nameOfDay.substr(0, 1)}
+//               selected={date}
+//               onChange={handleChange}
+//               inline
+//             />
+//           </DatePickerWrapper>
+//         )}
+//       </ForDatePicker>
+
+//       <Container>
+//         <ButtonDecrease className="decrease" onClick={getTasks}>
+//           <IconLeft height="9">
+//             <use href={icon + '#icon-chevron-left'}></use>
+//           </IconLeft>
+//         </ButtonDecrease>
+//         <ButtonIncrease className="increase" onClick={getTasks}>
+//           <IconRight height="9">
+//             <use href={icon + '#icon-chevron-right'}></use>
+//           </IconRight>
+//         </ButtonIncrease>
+//       </Container>
+
+//       <DatePickerWrapperStyles />
+//     </>
+//   );
+// };
+// export default PeriodPaginator;
+
+import React, { useState } from 'react'; // Додали useState
 import {
   ButtonIncrease,
   ButtonDecrease,
@@ -14,7 +102,7 @@ import icon from 'assets/icons/symbol-defs.svg';
 import { useLocation, useNavigate } from 'react-router-dom';
 import 'react-datepicker/dist/react-datepicker.css';
 import { enGB } from 'date-fns/locale';
-import { format } from 'date-fns';
+import { format, addMonths, subMonths } from 'date-fns'; // Додали addMonths та subMonths для зміни місяців
 
 registerLocale('enGB', enGB);
 
@@ -22,11 +110,15 @@ const PeriodPaginator = ({ date, getTasks, isOpen, setIsOpen, setDate }) => {
   const location = useLocation();
   const navigate = useNavigate();
 
+  // Додали стан для поточної дати
+  const [currentDate, setCurrentDate] = useState(date);
+
   const handleChange = e => {
     const formattedDate = format(e, 'yyyy-MM-dd');
 
     setIsOpen(!isOpen);
     setDate(e);
+    setCurrentDate(e); // Оновлюємо поточну дату
 
     if (location.pathname.startsWith('/calendar/month')) {
       navigate(`/calendar/month/${formattedDate}`);
@@ -36,12 +128,24 @@ const PeriodPaginator = ({ date, getTasks, isOpen, setIsOpen, setDate }) => {
       navigate(`/calendar/day/${formattedDate}`);
     }
   };
+
+  // Додали функцію для перемикання місяців та виклик функції getTasks
+  const handleIncreaseMonth = () => {
+    const newDate = addMonths(currentDate, 1);
+    setCurrentDate(newDate);
+    getTasks(newDate); // Викликаємо функцію getTasks для завантаження завдань нового місяця
+  };
+
+  const handleDecreaseMonth = () => {
+    const newDate = subMonths(currentDate, 1);
+    setCurrentDate(newDate);
+    getTasks(newDate); // Викликаємо функцію getTasks для завантаження завдань нового місяця
+  };
+
   const handleClick = e => {
     e.preventDefault();
     setIsOpen(!isOpen);
   };
-
-  // console.log(date);
 
   return (
     <>
@@ -49,8 +153,8 @@ const PeriodPaginator = ({ date, getTasks, isOpen, setIsOpen, setDate }) => {
         <Date className="example-custom-input" onClick={handleClick}>
           {location.pathname.startsWith('/calendar/day') ||
           location.pathname.startsWith('/statistics')
-            ? format(date, 'dd MMM yyyy')
-            : format(date, 'MMMM yyyy')}
+            ? format(currentDate, 'dd MMM yyyy') // Змінюємо на використання поточної дати
+            : format(currentDate, 'MMMM yyyy')}{' '}
         </Date>
         {isOpen && (
           <DatePickerWrapper>
@@ -58,7 +162,7 @@ const PeriodPaginator = ({ date, getTasks, isOpen, setIsOpen, setDate }) => {
               locale="enGB"
               onClickOutside={() => setIsOpen(!isOpen)}
               formatWeekDay={nameOfDay => nameOfDay.substr(0, 1)}
-              selected={date}
+              selected={currentDate} // Змінюємо на використання поточної дати
               onChange={handleChange}
               inline
             />
@@ -67,12 +171,13 @@ const PeriodPaginator = ({ date, getTasks, isOpen, setIsOpen, setDate }) => {
       </ForDatePicker>
 
       <Container>
-        <ButtonDecrease className="decrease" onClick={getTasks}>
+        {/* Додали обробники для кнопок перемикання місяців */}
+        <ButtonDecrease className="decrease" onClick={handleDecreaseMonth}>
           <IconLeft height="9">
             <use href={icon + '#icon-chevron-left'}></use>
           </IconLeft>
         </ButtonDecrease>
-        <ButtonIncrease className="increase" onClick={getTasks}>
+        <ButtonIncrease className="increase" onClick={handleIncreaseMonth}>
           <IconRight height="9">
             <use href={icon + '#icon-chevron-right'}></use>
           </IconRight>
